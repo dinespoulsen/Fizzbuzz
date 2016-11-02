2efd43639b0d1515d5b59d130da915bdeec30794
Dines Poulsen <dinespoulsen84@gmail.com>
2016-11-02 11:56:24 +0000

    made all tests work

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,4 +3,21 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+
+  it 'returns "fizz" when passed a number which is multiple of 3' do
+    expect(fizzbuzz(6)).to eq 'fizz'
+  end
+
+  it 'returns "buzz" when passed a number which is multiple of 5' do
+    expect(fizzbuzz(10)).to eq 'buzz'
+  end
+
+  it 'returns "fizzbuzz" when passed a number which is multiple of 3 and 5' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+
 end
